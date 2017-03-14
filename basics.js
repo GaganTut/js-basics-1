945a43bf820cbacd626484d295a8b6533ac9ecb3
GaganTut <gstut@live.com>
2017-03-13 16:00:52 -1000

    Finished Person Object

--- basics.js
+++ basics.js
@@ -4,7 +4,10 @@ var myName = "Gagan";
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
-
+var person = {
+  name: "Gagan",
+  age: 20
+};
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
